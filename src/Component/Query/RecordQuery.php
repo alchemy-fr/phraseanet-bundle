9e8c8b010ca5fa51cb9d82a6d3bc627a64bb15fd
felixalchemy <poimlk!>
2020-10-21 17:32:41 +0200

    PARADE-744 | Choose api version when using search route

--- src/Component/Query/RecordQuery.php
+++ src/Component/Query/RecordQuery.php
@@ -30,6 +30,16 @@ class RecordQuery
         return $this->query;
     }
 
+	/**
+	 * @param array $query
+	 * @return RecordQuery
+	 */
+	public function setRawQuery($query)
+	{
+		$this->query = $query;
+		return $this;
+	}
+
     /**
      * @return int
      */
@@ -40,15 +50,16 @@ class RecordQuery
 
     /**
      * @param $repository
+	 * @param int $pAPINumber API number (e.g. 3)
      * @return \Doctrine\Common\Collections\ArrayCollection
      */
-    public function execute($repository)
+    public function execute($repository, $pAPINumber = 1)
     {
         if (! $repository instanceof Record && ! $repository instanceof Story) {
             throw new \InvalidArgumentException('Invalid repository type (story or record required).');
         }
 
-        $result = $repository->search($this->query);
+        $result = $repository->search($this->query, $pAPINumber);
 
         if ($this->type == 1) {
             return $result->getResults()->getStories();
